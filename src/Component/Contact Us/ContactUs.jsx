7ffabf5b4231a78949accba5a09371d2b87d7301
Ahmed Helmy <115832929+5elmy@users.noreply.github.com>
2023-11-28 16:17:01 +0200

    upload25666

--- src/Component/Contact Us/ContactUs.jsx
+++ src/Component/Contact Us/ContactUs.jsx
@@ -23,15 +23,15 @@ export default function ContactUs() {
       <div className='grid grid-cols-12 gap-7 my-5'>
         <div className='col-span-12 md:col-span-4 shadow-md p-5 cursor-pointer'>
           <img src={san_francisco} alt="" />
-          <h3 className='font-bold text-[30px]'>San Francisco, CA</h3>
+          <h3 className='font-bold text-black text-[30px]'>San Francisco, CA</h3>
         </div>
         <div className='col-span-12 md:col-span-4 shadow-md p-5 cursor-pointer'>
           <img src={Istanbul} alt="" />
-          <h3 className='font-bold text-[30px]'>Istanbul, Türkiye</h3>
+          <h3 className='font-bold text-black text-[30px]'>Istanbul, Türkiye</h3>
         </div>
         <div className='col-span-12 md:col-span-4 shadow-md p-5 cursor-pointer'>
           <img src={denver} alt="" />
-          <h3 className='font-bold text-[30px]'>Denver, CO</h3>
+          <h3 className='font-bold text-black text-[30px]'>Denver, CO</h3>
         </div>
 
       </div>
